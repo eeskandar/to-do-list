1abc87a0589078d2b775dcfc875dd4c863371932
Iskandar <seik747@gmail.com>
2022-08-31 21:10:26 +0000

    fixing display problem with the close button

--- src/js/component/ToDo.jsx
+++ src/js/component/ToDo.jsx
@@ -3,8 +3,7 @@ import { useState } from "react";
 
 export const ToDo = () => {
 
-	const [stuffs, setStuffs] = useState('')
-	const [closing, setClosing] = useState(false)
+	const [stuffs, setStuffs] = useState("")
 	const [list, setList] = useState ([])
 	const [waiting, setWaiting] = useState (true)
 	
@@ -34,11 +33,10 @@ export const ToDo = () => {
 	// for mapping
 	const Mapping = list.map((task, i) => {
 		return (
-				<li key={i} className={`list-group-item col-md-8 col-lg-6 mx-auto border-0 mt-1 bg-dark text-white`} 
-				onMouseEnter={(event) => setClosing(true)} onMouseLeave={(event) => setClosing(false)}>
+				<li key={i} className="list-group-item col-sm-8 col-md-8 col-lg-6 mx-auto border-0 mt-1 bg-dark text-white show-button">
 					{task}
 					<button key={i} type="button"
-					className={`btn-close btn-close-white float-end ${closing? "" : "invisible"}`}
+					className="btn-close btn-close-white float-end invi-button"
 					onClick={(event) => deleteTask(i)}></button>
 				</li>
 		)
@@ -52,23 +50,23 @@ export const ToDo = () => {
 				<div className="row justify-content-center text-white fs-1 fst-italic fw-bolder">Is to Write Your ToDos"</div>
 				<div className="row text-white justify-content-center fst-italic">-Someone, at some point (maybe)-</div>
 			</div>
-			<div className="row col-md-8 col-lg-6 mx-auto">
+			<div className="row col-sm-8 col-md-8 col-lg-6 mx-auto">
 					<input id="tasker" 
 					type="text"
 					className={`list-group-item shadow border-0 ${waiting? "rounded" : "rounded-top"}`}
-					onChange={(event) => {setStuffs(event.target.value)}} 
+					onChange={(event) => {setStuffs(event.target.value)}}
 					value = {stuffs}
 					onKeyDown={submit}
 					placeholder="What do you need to do?"></input>
 			</div>
 			<div>
 				{waiting? (
-					<div className="text-white list-group-item col-md-8 col-lg-6 mx-auto border-0 disabled bg-transparent mt-1">Try adding a new task! 😎</div>
+					<div className="text-white list-group-item col-8 col-lg-6 mx-auto border-0 disabled bg-transparent mt-1">Try adding a new task! 😎</div>
 				) : (
 				<ul className="p-0">
 					{Mapping}
-					<div className="list-end col-md-8 col-lg-6 mx-auto text-white rounded-bottom shadow"></div>
-					<div className="text-white list-group-item col-md-8 col-lg-6 mx-auto border-0 disabled bg-transparent mt-1">I know you can do it! Only {list.length} left to go!</div>
+					<div className="list-end col-8 col-lg-6 mx-auto text-white rounded-bottom shadow"></div>
+					<div className="text-white list-group-item col-8 col-lg-6 mx-auto border-0 disabled bg-transparent mt-1">I know you can do it! Only {list.length} left to go!</div>
 				</ul>)}
 			</div>
 		</div>
